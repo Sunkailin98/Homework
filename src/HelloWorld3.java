62e1ec651b36e8f639448c1ee798e31afea6c5de
Sunny <52986102+Sunkailin98@users.noreply.github.com>
2019-08-01 21:01:51 +0800

    Update HelloWorld3.java

--- src/HelloWorld3.java
+++ src/HelloWorld3.java
@@ -11,7 +11,65 @@
         }
         Result: "cat", "dog", "red"
 */
+import java.util.*;
 
+public class HelloWorld3 {
+
+
+
+    public static String[] strs = new String[3];
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        for (int i = 0; i < strs.length; i++) {
+            System.out.println("请输入第" + (i + 1) + "个单词:");
+            String str = input.nextLine();
+            strs[i] = str;
+        }
+
+
+        show(getMaxValuesAndIndexes(getLengths()));
+
+    }
+
+    public static int[] getLengths(){
+        int[] index = new int[3];
+        for(int i = 0;i<strs.length;i++){
+            index[i]=strs[i].length();
+        }
+        return index;
+    }
+    public static String[] getMaxValuesAndIndexes(int[] arr) {
+        Map<String, Object> map = new HashMap<>();
+        int max = arr[0];
+        map.put("indexes", 0);
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            if (max == temp) {
+                map.put("indexes", map.get("indexes") + "," + i);
+            } else if (max < temp) {
+                max = temp;
+                map.put("indexes", i);
+            }
+        }
+        String[] indexesArray = map.get("indexes").toString().split(",");
+        return indexesArray;
+    }
+
+    public static void show(String[] args){
+        System.out.println("Result:");
+        for (int i = 0; i <args.length ; i++) {
+
+            System.out.println(strs[Integer.parseInt(args[i])]);
+        }
+
+    }
+}
+
+
+/*
 import java.util.*;
 import java.lang.String ;
 
@@ -38,6 +96,7 @@ public class HelloWorld3 {
     }
 
 }
+*/
     /*
     public static void main(String[] args) {
 
